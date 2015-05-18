8d339a3232f839fff1c25cac11e7ae59373326b9
sunli <sunli@frontnode.net>
2015-05-18 15:14:47 +0800

    使用配置文件管理mybatis的SQL

--- src/main/java/mapper/VillageMapper.java
+++ src/main/java/mapper/VillageMapper.java
@@ -9,10 +9,10 @@ import org.apache.ibatis.annotations.Select;
  * Created by jiankuan on 15/5/15.
  */
 public interface VillageMapper {
-    @Insert("INSERT INTO village(name, district) VALUES(#{name}, #{district})")
-    @Options(useGeneratedKeys=true, keyProperty="vid") // return generated key
+//    @Insert("INSERT INTO village(name, district) VALUES(#{name}, #{district})")
+//    @Options(useGeneratedKeys=true, keyProperty="vid") // return generated key
     void insertVillage(Village village);
 
-    @Select("SELECT vid, name, district FROM village WHERE vid = #{vid}")
+//    @Select("SELECT vid, name, district FROM village WHERE vid = #{vid}")
     Village getVillage(int vid);
 }
