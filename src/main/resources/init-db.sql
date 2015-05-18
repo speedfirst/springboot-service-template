3babc58c64b8c3cc332bb7b164395c3c5ea51bd2
jiankuan.xing <jiankuan.xing@renren-inc.co>
2015-05-18 15:51:07 +0800

    fix duplicated primary key in init-db.sql

--- src/main/resources/init-db.sql
+++ src/main/resources/init-db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `village` (
-  `vid` int(10) PRIMARY KEY AUTO_INCREMENT,
+  `vid` int(10) AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `district` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`vid`)
