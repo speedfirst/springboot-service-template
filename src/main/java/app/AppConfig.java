a3592ebce9db57cfb9948730069069d97e3c06d9
jiankuan.xing <jiankuan.xing@renren-inc.co>
2015-05-18 17:22:25 +0800

    use tomcat db connection pool, remove commons-dbcp

--- src/main/java/app/AppConfig.java
+++ src/main/java/app/AppConfig.java
@@ -1,9 +1,9 @@
 package app;
 
-import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,25 +21,18 @@ import javax.sql.DataSource;
 @MapperScan("mapper")  // scan mappers for mybatis
 public class AppConfig {
 
-    @Bean
-    public DataSource getDataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        return dataSource;
-    }
+    @Autowired
+    private DataSource dataSource; // data source will be automatically injected, see application.properties
 
     @Bean
     public DataSourceTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(getDataSource());
+        return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(getDataSource());
+        sessionFactory.setDataSource(dataSource);
         return sessionFactory.getObject();
     }
 }
