d31b5216d9aecc8bedf81529a4822249403e6e54
jiankuan.xing <jiankuan.xing@renren-inc.co>
2015-05-18 16:53:31 +0800

    add insertVillage mybatis bounding

--- src/test/java/app/HelloControllerTest.java
+++ src/test/java/app/HelloControllerTest.java
@@ -33,6 +33,7 @@ public class HelloControllerTest {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+				.andExpect(content().string(equalTo("Hello, this is a web app based on springboot\n"
+						)));
 	}
 }
