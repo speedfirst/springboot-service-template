b267c8eb409c2352347f2687d71895daac2a7b0f
邢剑宽 <jiankuan@frontnode.net>
2015-05-30 17:10:59 +0800

    fix builder script

--- src/main/docker/build-docker-img.sh
+++ src/main/docker/build-docker-img.sh
@@ -2,14 +2,16 @@
 
 DOCKER_REGISTRY=reg.frontnode.net
 APP_NAME=${project.build.finalName}
-IMAGE_NAME=$DOCKER_REGISTRY/app/$APP_NAME
 
-def build() {
+# docker image name MUST be lower case
+IMAGE_NAME=`echo $DOCKER_REGISTRY/app/$APP_NAME | tr [:upper:] [:lower:]`
+
+function build() {
     echo 'building docker image [$IMAGE_NAME]'
     docker build -t $IMAGE_NAME ${project.build.directory}
 }
 
-def push() {
+function push() {
     echo 'pushing docker image [$IMAGE_NAME] to [$DOCKER_REGISTRY]'
     docker push $IMAGE_NAME
 }
