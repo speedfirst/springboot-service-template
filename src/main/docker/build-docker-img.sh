f097d242c5950f86ab5a809adb18e24c4d4cc900
jenkins <jenkins@frontnode.net>
2015-06-01 14:44:48 +0800

    u

--- src/main/docker/build-docker-img.sh
+++ src/main/docker/build-docker-img.sh
@@ -6,11 +6,5 @@ APP_NAME=${project.build.finalName}
 # docker image name MUST be lower case
 IMAGE_NAME=$(echo $DOCKER_REGISTRY/app/$APP_NAME | tr [:upper:] [:lower:])
 
-function build()
-{
-    echo "building docker image [$1]"
-    docker build -t $1 ${project.build.directory}
-}
-
-# do the build
-build $IMAGE_NAME
+echo "building docker image [$IMAGE_NAME]"
+docker build -t $IMAGE_NAME ${project.build.directory}
