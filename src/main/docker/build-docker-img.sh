69aac5d62badf47e47b265b37f78d15a391ce648
李晶 <lijing@frontnode.net>
2015-06-04 21:25:55 +0800

    update

--- src/main/docker/build-docker-img.sh
+++ src/main/docker/build-docker-img.sh
@@ -6,6 +6,7 @@ APP_NAME=${project.build.finalName}
 # docker image name MUST be lower case
 IMAGE_NAME=$(echo $DOCKER_REGISTRY/app/$APP_NAME | tr [:upper:] [:lower:])
 
+echo " " >> $(dirname $0)/build-result.properties
 echo "IMAGE_NAME=$IMAGE_NAME" >> $(dirname $0)/build-result.properties
 
 echo "building docker image [$IMAGE_NAME]"
